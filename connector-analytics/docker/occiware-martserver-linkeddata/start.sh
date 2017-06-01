02d176d74d9a351e5c6e117b4148fd578d90871a
xia0ben <benoit.renault@smile.fr>
2017-06-01 16:17:02 +0200

    Update scripts for LinkedData-MartServer dockerization

--- connector-analytics/docker/occiware-martserver-linkeddata/start.sh
+++ connector-analytics/docker/occiware-martserver-linkeddata/start.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
 # Workaround for Docker Studio since it doesn't support the --add-host parameter yet :
-# Add mongo containers IPs to /etc/hosts file so that the datacore be able to connect with them
-#echo 172.17.0.2 ozwillo-mongo-1 >> /etc/hosts
-#echo 172.17.0.3 ozwillo-mongo-2 >> /etc/hosts
-#echo 172.17.0.4 ozwillo-mongo-3 >> /etc/hosts
+# Add datacore container IP to /etc/hosts file so that the datacore be able to connect with it
+echo 172.17.0.5 ozwillo-datacore-1 >> /etc/hosts
 
-# Start the Tomcat webserver
-while true; do
-    echo Hello World
-    sleep 1
-done
+# Start the MartServer
+mvn exec:java -Dexec.args=martserver.config
