070d5c6ea3b4410605ee6ff8a45302d6a463a009
xia0ben <benoit.renault@smile.fr>
2017-06-13 14:47:27 +0200

    Update Martserver LinkedData Dockerization

--- connector-analytics/docker/occiware-martserver-linkeddata/deploy.sh
+++ connector-analytics/docker/occiware-martserver-linkeddata/deploy.sh
@@ -8,34 +8,27 @@ apt-get update \
 # Get the MartServer, version 1.5 (which explains the git checkout)
 cd /tmp
 git clone https://github.com/occiware/MartServer.git
-cd MartServer
+cd /tmp/MartServer
 git checkout 20b454504d91f6390afa37f0e5f62044ecad7726
 mvn initialize
 mvn clean install
-cd /tmp
-rm -rf MartServer
 
 # Get the latest version of the Datacore
 cd /tmp
 git clone https://github.com/ozwillo/ozwillo-datacore.git
-cd ozwillo-datacore
+cd /tmp/ozwillo-datacore
 mvn clean install -DskipTests
-cd /tmp
-rm -rf ozwillo-datacore
 
 # Get the LinkedData Extension and Connector
 cd /tmp
-git clone https://github.com/occiware/ecore.git
 # ---- STARTOF : TO BE UPDATED WHEN LINKEDDATA CHANGES COMMITED TO ECORE ----
-git clone https://github.com/xia0ben/occiware-ozwillo.git
-cp -rf occiware-ozwillo/connector-analytics/org.occiware.clouddesigner.occi.linkeddata* ecore/clouddesigner/
+git clone https://github.com/xia0ben/ecore.git
 # ---- ENDOF : TO BE UPDATED WHEN LINKEDDATA CHANGES COMMITED TO ECORE ----
-cd ecore/clouddesigner/org.occiware.clouddesigner.parent
+cd /tmp/ecore/clouddesigner/org.occiware.clouddesigner.parent
 mvn clean install
-cd /tmp
-rm -rf occiware-ozwillo
-rm -rf ecore
-#cd ecore
+
+# Destroy all dependency sources
+rm -rf /tmp/*
 
 # Build the app
 cd /app
