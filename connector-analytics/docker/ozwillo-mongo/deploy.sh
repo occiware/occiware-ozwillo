559099e36330ec95dbdeb893518a66b96d6e198e
xia0ben <benoit.renault@smile.fr>
2017-06-21 14:00:07 +0200

    Update Mongo Dockerization

--- connector-analytics/docker/ozwillo-mongo/deploy.sh
+++ connector-analytics/docker/ozwillo-mongo/deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# CONFIGURATION VARIABLES (you might need to change them !)
-is_mongo_master=true
-
 # Export necessary ENV variables
 export MONGO_VERSION=2.6.5
 
@@ -15,13 +12,3 @@ echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' |
 apt-get update
 apt-get install -y --no-install-recommends adduser mongodb-org-server=$MONGO_VERSION mongodb-org-shell=$MONGO_VERSION
 rm -rf /var/lib/apt/lists/*
-
-# If this is the master mongo, start mongod to configure it
-
-# ...do something interesting...
-if [ "$the_world_is_flat" = true ] ; then
-    echo 'Be careful not to fall off!'
-fi
-/usr/bin/mongod --smallfiles --noprealloc --replSet rs0 --logpath /var/log/mongodb/mongod.log --dbpath /data/db
-
-mongo < /tmp/master_deploy.js
--- connector-analytics/docker/ozwillo-mongo/master_deploy.js
+++ connector-analytics/docker/ozwillo-mongo/master_deploy.js
@@ -1,16 +1,35 @@
-use admin
-rs.initiate()
-// Using IP since studio can not yet --add-host in both ways using links
-// If ozwillo-mongo-1 has IP adress 172.17.0.2, if you did rs.add("172.17.0.2:27017"), it would raise error 13433 exception: can not find self in new replset config.
-rs.add("172.17.0.3:27017")
-rs.add("172.17.0.4:27017")
-cfg = rs.conf()
-// Replaces the docker-gen of this host by ip https://sebastianvoss.com/docker-mongodb-sharded-cluster.html
-cfg.members[0].host = "172.17.0.2:27017"
-// Prevents ozwillo-mongo-2 from becoming master :
-// (so that it can be targeted as custom secondary by an LDDatabaseLink without risk)
-// https://docs.mongodb.com/v2.6/tutorial/configure-secondary-only-replica-set-member/
-cfg.members[1].priority = 0
-rs.reconfig(cfg)
-rs.status()
-// check if rs.status() says all other replica are SECONDARY
+// If and only if replicaSet status has not been set up yet
+if (rs.status().startupStatus === 3) {
+    db = db.getSiblingDB('admin');
+    
+    rs.initiate();
+    print("RS status after initiate :");
+    printjson(rs.status());
+    // Using IP since studio can not yet --add-host in both ways using links
+    // If ozwillo-mongo-1 has IP adress 172.17.0.2, if you did
+    // rs.add("172.17.0.2:27017"), it would raise error 13433 exception: can
+    // not find self in new replset config.
+    rs.add("ozwillo-mongo-2:27017");
+    rs.add("ozwillo-mongo-3:27017");
+    print("RS status after adding ozwillo-mongo-2/3 :");
+    printjson(rs.status());
+
+    // Replaces the docker-gen of this host by ip :
+    // https://sebastianvoss.com/docker-mongodb-sharded-cluster.html
+    cfg = rs.conf();
+    cfg.members[0].host = "ozwillo-mongo-1:27017";
+    print("RS status after changing cfg.members[0].host to ozwillo-mongo-1 :");
+    printjson(rs.status());
+    // Prevents ozwillo-mongo-2 from becoming master :
+    // (so that it can be targeted as custom secondary by an LDDatabaseLink
+    // without risk) :
+    // https://docs.mongodb.com/v2.6/tutorial/configure-secondary-only-replica-set-member/
+    cfg.members[1].priority = 0;
+    print("RS status after changing cfg.members[1].priority to ozwillo-mongo-1 :");
+    printjson(rs.status());
+    rs.reconfig(cfg);
+    rs.status();
+    print("Final RS status :");
+    printjson(rs.status());
+    // check if rs.status() says all other replica are SECONDARY
+}
