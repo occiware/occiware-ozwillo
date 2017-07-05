a8d4b5fa3939bd5bc162a85cf08b8362df3a898c
xia0ben <benoit.renault@smile.fr>
2017-07-05 16:57:05 +0200

    Add remote debug capabilities for datacore container

--- connector-analytics/docker/ozwillo-datacore/start.sh
+++ connector-analytics/docker/ozwillo-datacore/start.sh
@@ -6,5 +6,9 @@ echo 172.17.0.2 ozwillo-mongo-1 >> /etc/hosts
 echo 172.17.0.3 ozwillo-mongo-2 >> /etc/hosts
 echo 172.17.0.4 ozwillo-mongo-3 >> /etc/hosts
 
+# Allow remote debug
+export CATALINA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=8500,server=y,suspend=n"
+export JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=8500,server=y,suspend=n"
+
 # Start the Tomcat webserver
 catalina.sh run
