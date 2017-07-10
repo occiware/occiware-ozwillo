7432166f92f7a2e10b6f860c88e68a92a136187c
xia0ben <benoit.renault@smile.fr>
2017-07-10 17:36:45 +0200

    Create specific docker files for ozwillo-mongo-master-with-data

---
+++ docker/ozwillo-mongo-master-with-data/master_deploy.js
@@ -0,0 +1,68 @@
+function waitforDB(hostAndPort) {
+    var conn;
+    do
+    {
+        try
+        {
+            conn = new Mongo(hostAndPort);
+        }
+        catch(Error)
+        {
+            //print(Error);
+        }
+        sleep(100);
+    } while(conn===undefined);
+}
+
+// If and only if replicaSet status has not been set up yet
+if (rs.status().startupStatus === 3) {
+    db = db.getSiblingDB('admin');
+
+    // Initiate and wait for initialization to finish
+    rs.initiate();
+    while (rs.status().members == null
+        || rs.status().members[0].stateStr !== "PRIMARY") {}
+    print("RS status after initiate :");
+    printjson(rs.status());
+
+    // Using IP since studio can not yet --add-host in both ways using links
+    // If ozwillo-mongo-1 has IP adress 172.17.0.2, if you did
+    // rs.add("172.17.0.2:27017"), it would raise error 13433 exception: can
+    // not find self in new replset config.
+    waitforDB("ozwillo-mongo-2:27017");
+    rs.add("ozwillo-mongo-2:27017");
+    while (rs.status().members[1] == null
+        || rs.status().members[1].stateStr !== "SECONDARY") {
+        sleep(100);
+    }
+    print("RS status after adding ozwillo-mongo-2 :");
+    printjson(rs.status());
+
+    waitforDB("ozwillo-mongo-3:27017");
+    rs.add("ozwillo-mongo-3:27017");
+    while (rs.status().members[2] == null
+        || rs.status().members[2].stateStr !== "SECONDARY") {
+        sleep(100);
+    }
+    print("RS status after adding ozwillo-mongo-3 :");
+    printjson(rs.status());
+
+    // Replaces the docker-gen of this host by ip :
+    // https://sebastianvoss.com/docker-mongodb-sharded-cluster.html
+    cfg = rs.conf();
+    cfg.members[0].host = "ozwillo-mongo-1:27017";
+    print("RS status after changing cfg.members[0].host to ozwillo-mongo-1 :");
+    printjson(rs.status());
+    // Prevents ozwillo-mongo-2 from becoming master :
+    // (so that it can be targeted as custom secondary by an LDDatabaseLink
+    // without risk) :
+    // https://docs.mongodb.com/v2.6/tutorial/configure-secondary-only-replica-set-member/
+    cfg.members[1].priority = 0;
+    print("RS status after changing cfg.members[1].priority to ozwillo-mongo-1 :");
+    printjson(rs.status());
+    rs.reconfig(cfg);
+    rs.status();
+    print("Final RS status :");
+    printjson(rs.status());
+    // check if rs.status() says all other replica are SECONDARY
+}
---
+++ docker/ozwillo-mongo-master-with-data/start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Workaround for Docker Studio since it doesn't support the --add-host parameter yet :
+# Add other containers IP to /etc/hosts file
+echo 172.17.0.2 ozwillo-mongo-1 >> /etc/hosts
+echo 172.17.0.3 ozwillo-mongo-2 >> /etc/hosts
+echo 172.17.0.4 ozwillo-mongo-3 >> /etc/hosts
+
+# Launch Mongo Daemon : No --auth, though --reset could be useful here.
+/usr/bin/mongod --smallfiles --fork --noprealloc --replSet rs0 --logpath /var/log/mongodb/mongod.log --dbpath /data/db
+
+mongo < /root/master_deploy.js
+
+# Check if the datacore db has already been restored : if not, restore it
+DBNAME=datacore
+if [ ! -e "/data/db/$DBNAME.ns" ]; then
+    # Get the dump
+    cd /tmp
+    git clone https://github.com/Xia0ben/occiware-ozwillo.git
+
+    # Restore the mongo dump
+    cd /tmp/occiware-ozwillo/mongo-dumps/
+    tar xzf energy_fixed.tar.gz
+    mongorestore --db datacore energy_fixed/dump/datacore/
+
+    # Empty tmp directory
+    rm -rf /tmp/*
+fi
+
+while :; do sleep 100; done
