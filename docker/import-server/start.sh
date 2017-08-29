cfb317045af8e3754fa3b58c4e1a0d2efa8214c6
xia0ben <benoit.renault@smile.fr>
2017-08-29 17:23:02 +0200

    Update import-server start.sh

--- docker/import-server/start.sh
+++ docker/import-server/start.sh
@@ -6,6 +6,6 @@ echo 172.17.0.5 ozwillo-datacore-1 >> /etc/hosts
 
 # Start the Tomcat webserver
 cd /app/ozwillo-ozenergy/ozwillo-ozenergy-data
-mvn clean test -Dtest=DatacoreEnergyImportTest
+mvn clean test -Dtest=DatacoreEnergyImportClientTest
 
 while :; do sleep 100; done
