d7275166423ecd0ca368bc7217f495c082ae9de2
xia0ben <benoit.renault@smile.fr>
2017-08-30 15:12:06 +0200

    Update import-server deploy.sh

--- docker/import-server/deploy.sh
+++ docker/import-server/deploy.sh
@@ -4,6 +4,17 @@
 apt-get update
 apt-get install -y git default-jdk maven
 
+## PostgreSQL installation
+# Add the PostgreSQL PGP key to verify their Debian packages.
+# It should be the same key as https://www.postgresql.org/media/keys/ACCC4CF8.asc
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys B97B0AFCAA1A47F044F244A07FCC7D46ACCC4CF8
+# Add PostgreSQL's repository. It contains the most recent stable release
+#     of PostgreSQL, ``9.6``.
+echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+apt-get update
+apt-get -y -q install software-properties-common
+apt-get -y -q install postgresql-9.6 postgresql-client-9.6 postgresql-contrib-9.6
+
 # Build the the data project
 cd /app
 git clone https://github.com/ozwillo/ozwillo-ozenergy.git
