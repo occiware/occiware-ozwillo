a83e457e5140c0d76e24af4cab9106e686fcf5c3
xia0ben <benoit.renault@smile.fr>
2017-08-31 18:19:51 +0200

    Fix import-server start.sh

--- docker/import-server/start.sh
+++ docker/import-server/start.sh
@@ -4,12 +4,11 @@
 # Add mongo containers IPs to /etc/hosts file so that the datacore be able to connect with them
 echo 172.17.0.5 ozwillo-datacore-1 >> /etc/hosts
 
-# Start postgresql TODO DO IN BACK
-su -s /bin/bash postgres
-/usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf >/dev/null 2>&1 &
-exit
+# Start postgresql
+su postgres -c '/usr/lib/postgresql/9.6/bin/postgres -D /var/lib/postgresql/9.6/main -c config_file=/etc/postgresql/9.6/main/postgresql.conf >/dev/null 2>&1 &'
+sleep 30
 
-# Start Blynk TODO DO IN BACK
+# Start Blynk
 java -jar /blynk/server.jar -dataFolder /data -serverConfig /config/server.properties >/dev/null 2>&1 &
 
 # Start the Tomcat webserver
